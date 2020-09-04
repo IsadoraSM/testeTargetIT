dac393f533fff6897683161dfe04899c56f677fd
ialisadora <isadorasoaresmotta@gmail.com>
2020-09-04 15:34:42 -0300

    Adiciona botão de registrar no menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,11 @@
                                 </li>
                             @endif
                         @else
+                            @if(Auth::user()->profile->name == "Master")
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Registrar novo usuário</a>
+                                </li>
+                            @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
